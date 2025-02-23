6a076c5830e19f25d5c9a793a0cb75309adec8d7
DS <dariusz.steblik@grandparade.co.uk>
2025-02-24 00:22:06 +0100

    Initial commit - Todo app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TodoList from './components/TodoList';
+
+function App() {
+  
+  return (
+    <div>
+      <h1>My To-Do list</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App
