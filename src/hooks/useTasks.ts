5be1842a8acef8ae2e829225148f1556974c4922
Dariusz Steblik <dariusz.steblik@grandparade.co.uk>
2026-01-08 22:29:57 +0100

    [TASK-005] Use reduser in useTask hook

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,31 +1,47 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { Task } from '../types/task';
 
+type TaskAction =
+  | { type: 'ADD_TASK'; payload: { text: string } }
+  | { type: 'TOGGLE_TASK'; payload: { id: number } }
+  | { type: 'DELETE_TASK'; payload: { id: number } };
+
+const taskReducer = (state: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case 'ADD_TASK': {
+      return [
+        ...state,
+        { id: Date.now(), text: action.payload.text, completed: false },
+      ];
+    }
+    case 'TOGGLE_TASK': {
+      return state.map((task) =>
+        task.id === action.payload.id
+          ? { ...task, completed: !task.completed }
+          : task,
+      );
+    }
+    case 'DELETE_TASK': {
+      return state.filter((task) => task.id !== action.payload.id);
+    }
+    default:
+      return state;
+  }
+};
+
 export const useTasks = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, dispatch] = useReducer(taskReducer, []);
 
   const addTask = (text: string) => {
-    const newTask: Task = {
-      id: Date.now(),
-      text,
-      completed: false,
-    };
-
-    setTasks((previousTasks) => [...previousTasks, newTask]);
+    dispatch({ type: 'ADD_TASK', payload: { text } });
   };
 
-  const toggleTask = (taskId: number) => {
-    setTasks((previousTask) =>
-      previousTask.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task,
-      ),
-    );
+  const toggleTask = (id: number) => {
+    dispatch({ type: 'TOGGLE_TASK', payload: { id } });
   };
 
-  const deleteTask = (taskId: number) => {
-    setTasks((previousTasks) =>
-      previousTasks.filter((task) => task.id !== taskId),
-    );
+  const deleteTask = (id: number) => {
+    dispatch({ type: 'DELETE_TASK', payload: { id } });
   };
 
   return { tasks, addTask, toggleTask, deleteTask };
